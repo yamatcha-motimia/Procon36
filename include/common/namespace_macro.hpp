f25df921374d47ac595f43c1feccdf6de278e738
yamatcha-motimia <yamatcha.pixelforge@gmail.com>
2025-10-26 15:28:57 +0900

    [add] timer.cpp

---
+++ include/common/namespace_macro.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+// 他ソルバとの競合防止用
+#define BEGIN_SOLVER_NAMESPACE namespace solver {
+#define   END_SOLVER_NAMESPACE }
---
+++ include/util/timer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "common/namespace_macro.hpp"
+
+BEGIN_SOLVER_NAMESPACE
+
+class Timer {
+    int start();
+}
+
+END_SOLVER_NAMESPACE
